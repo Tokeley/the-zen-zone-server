32c9694801f196080504493e6398caa6874d2ef5
Alex Manning <alex.t.manning@gmail.com>
2024-06-28 22:37:42 +1200

    fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors'); // Add this line to import the cors package
 const soundscapeRoutes = require('./routes/soundscapes')
 const userRoutes = require('./routes/user')
 const mixRoutes = require('./routes/mixes')
